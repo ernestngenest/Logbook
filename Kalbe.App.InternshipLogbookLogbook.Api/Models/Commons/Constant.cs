1a0af747391b42c949d3ae6c3de9938830b85ed7
blackrafale <149214037+blackrafale@users.noreply.github.com>
2023-11-21 09:30:56 +0700

    Revert "Add project files."
    This reverts commit f673bf8bb5588c8afb3bd39857e1fa384fbefd99.

--- Kalbe.App.InternshipLogbookLogbook.Api/Models/Commons/Constant.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Kalbe.App.InternshipLogbookLogbook.Api.Models.Commons
-{
-    public static class Constant
-    {
-        public static readonly string SystemCode = "{SYSTEMCODE}";
-        public static readonly string ModuleCode = "{MODULECODE}";
-    }
-}
